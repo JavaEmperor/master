b45a0cf86243e1d840082f644b1ce49c1237898c
z_praise <z_praise@163.com>
2020-02-17 12:23:52 +0800

    打包配置

--- admin-ui/config/dev.env.js
+++ admin-ui/config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NODE_ENV: '"development"',
     ENV_CONFIG: '"dev"',
-    BASE_API: '"http://localhost:8083/manager"'
+    BASE_API: '"http://114.116.48.153:8073/admin"'
 }
--- admin-ui/config/prod.env.js
+++ admin-ui/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
-	NODE_ENV: '"production"',
-	ENV_CONFIG: '"prod"',
-    BASE_API: '"https://www.xxx.com/manager"'
+  NODE_ENV: '"production"',
+  ENV_CONFIG: '"prod"',
+  BASE_API: '"http://114.116.48.153:8073/admin"'
 }
