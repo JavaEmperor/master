2181badeb2980743d4d83bca7d279f6f437f8fad
z_praise <z_praise@163.com>
2020-02-19 11:44:07 +0800

    代码编译规范

--- admin-ui/.eslintrc.js
+++ admin-ui/.eslintrc.js
@@ -35,7 +35,7 @@ module.exports = {
       'properties': 'always'
     }],
     'comma-dangle': [2, 'never'],
-    'comma-spacing': [2, {
+    'comma-spacing': [0, {
       'before': false,
       'after': true
     }],
@@ -50,14 +50,21 @@ module.exports = {
       'after': true
     }],
     'handle-callback-err': [2, '^(err|error)$'],
-    'indent': [2, 2, {
+    'indent': [1, 2, {
       'SwitchCase': 1
     }],
     'jsx-quotes': [2, 'prefer-single'],
-    'key-spacing': [2, {
-      'beforeColon': false,
-      'afterColon': true
-    }],
+    'key-spacing': [0, {
+      'singleLine': {
+        'beforeColon': false,
+        'afterColon': true
+      },
+      "multiLine": {
+        "beforeColon": true,
+        "afterColon": true,
+        "align": "colon"
+      },
+     }],
     'keyword-spacing': [2, {
       'before': true,
       'after': true
@@ -67,129 +74,124 @@ module.exports = {
       'capIsNew': false
     }],
     'new-parens': 2,
-    'no-array-constructor': 2,
-    'no-caller': 2,
-    'no-console': 'off',
-    'no-class-assign': 2,
-    'no-cond-assign': 2,
-    'no-const-assign': 2,
-    'no-control-regex': 0,
-    'no-delete-var': 2,
-    'no-dupe-args': 2,
+    'no-array-constructor': 2,//禁止使用数组构造器
+    'no-caller': 2,//禁止使用arguments.caller或arguments.callee
+    'no-console': 'off',//禁止使用console
+    'no-class-assign': 2,//禁止给类赋值
+    'no-cond-assign': 2,//禁止在条件表达式中使用赋值语句
+    'no-const-assign': 2,//禁止修改const声明的变量
+    'no-control-regex': 0,//禁止在正则表达式中使用控制字符
+    'no-delete-var': 2,//不能对var声明的变量使用delete操作符
+    'no-dupe-args': 2,//函数参数不能重复
     'no-dupe-class-members': 2,
-    'no-dupe-keys': 2,
-    'no-duplicate-case': 2,
-    'no-empty-character-class': 2,
+    'no-dupe-keys': 2,//在创建对象字面量时不允许键重复 {a:1,a:1}
+    'no-duplicate-case': 2,//switch中的case标签不能重复
+    'no-empty-character-class': 2,//正则表达式中的[]内容不能为空
     'no-empty-pattern': 2,
-    'no-eval': 2,
-    'no-ex-assign': 2,
-    'no-extend-native': 2,
-    'no-extra-bind': 2,
-    'no-extra-boolean-cast': 2,
-    'no-extra-parens': [2, 'functions'],
-    'no-fallthrough': 2,
-    'no-floating-decimal': 2,
-    'no-func-assign': 2,
-    'no-implied-eval': 2,
-    'no-inner-declarations': [2, 'functions'],
-    'no-invalid-regexp': 2,
-    'no-irregular-whitespace': 2,
-    'no-iterator': 2,
-    'no-label-var': 2,
+    'no-eval': 2,//禁止使用eval
+    'no-ex-assign': 2,//禁止给catch语句中的异常参数赋值
+    'no-extend-native': 2,//禁止扩展native对象
+    'no-extra-bind': 2,//禁止不必要的函数绑定
+    'no-extra-boolean-cast': 2,//禁止不必要的bool转换
+    'no-extra-parens': [2, 'functions'],//禁止非必要的括号
+    'no-fallthrough': 2,//禁止switch穿透
+    'no-floating-decimal': 2,//禁止省略浮点数中的0 .5 3.
+    'no-func-assign': 2,//禁止重复的函数声明
+    'no-implied-eval': 2,//禁止使用隐式eval
+    'no-inner-declarations': [2, 'functions'],//禁止在块语句中使用声明（变量或函数）
+    'no-invalid-regexp': 2,//禁止无效的正则表达式
+    'no-irregular-whitespace': 2,//不能有不规则的空格
+    'no-iterator': 2,//禁止使用__iterator__ 属性
+    'no-label-var': 2,//label名不能与var声明的变量名相同
     'no-labels': [2, {
       'allowLoop': false,
       'allowSwitch': false
-    }],
-    'no-lone-blocks': 2,
-    'no-mixed-spaces-and-tabs': 2,
-    'no-multi-spaces': 2,
-    'no-multi-str': 2,
-    'no-multiple-empty-lines': [2, {
-      'max': 1
-    }],
-    'no-native-reassign': 2,
-    'no-negated-in-lhs': 2,
-    'no-new-object': 2,
-    'no-new-require': 2,
+    }],//禁止标签声明
+    'no-lone-blocks': 2,//禁止不必要的嵌套块
+    'no-mixed-spaces-and-tabs': 0,//禁止混用tab和空格
+    'no-multi-spaces': 1,//不能用多余的空格
+    'no-multi-str': 2,//字符串不能用\换行
+    'no-multiple-empty-lines': [0, {
+      'max': 100
+    }],//空行最多不能超过1行
+    'no-native-reassign': 2,//不能重写native对象
+    'no-negated-in-lhs': 2,//in 操作符的左边不能有!
+    'no-new-object': 2,//禁止使用new Object()
+    'no-new-require': 2,//禁止使用new require
     'no-new-symbol': 2,
-    'no-new-wrappers': 2,
-    'no-obj-calls': 2,
-    'no-octal': 2,
-    'no-octal-escape': 2,
-    'no-path-concat': 2,
-    'no-proto': 2,
-    'no-redeclare': 2,
-    'no-regex-spaces': 2,
-    'no-return-assign': [2, 'except-parens'],
+    'no-new-wrappers': 2,//禁止使用new创建包装实例，new String new Boolean new Number
+    'no-obj-calls': 2,//不能调用内置的全局对象，比如Math() JSON()
+    'no-octal': 2,//禁止使用八进制数字
+    'no-octal-escape': 2,//禁止使用八进制转义序列
+    'no-path-concat': 2,//node中不能使用__dirname或__filename做路径拼接
+    'no-proto': 2,//禁止使用__proto__属性
+    'no-redeclare': 2,//禁止重复声明变量
+    'no-regex-spaces': 2,//禁止在正则表达式字面量中使用多个空格 /foo bar/
+    'no-return-assign': [2, 'except-parens'],//return 语句中不能有赋值表达式
     'no-self-assign': 2,
-    'no-self-compare': 2,
-    'no-sequences': 2,
-    'no-shadow-restricted-names': 2,
-    'no-spaced-func': 2,
-    'no-sparse-arrays': 2,
-    'no-this-before-super': 2,
-    'no-throw-literal': 2,
-    'no-trailing-spaces': 2,
-    'no-undef': 2,
-    'no-undef-init': 2,
-    'no-unexpected-multiline': 2,
+    'no-self-compare': 2,//不能比较自身
+    'no-sequences': 0,//禁止使用逗号运算符
+    'no-shadow-restricted-names': 2,//严格模式中规定的限制标识符不能作为声明时的变量名使用
+    'no-spaced-func': 2,//函数调用时 函数名与()之间不能有空格
+    'no-sparse-arrays': 2,//禁止稀疏数组， [1,,2]
+    'no-this-before-super': 2,//在调用super()之前不能使用this或super
+    'no-throw-literal': 2,//禁止抛出字面量错误 throw "error";
+    'no-trailing-spaces': 0,//一行结束后面不要有空格
+    'no-undef': 1,//不能有未定义的变量
+    'no-undef-init': 2,//变量初始化时不能直接给它赋值为undefined
+    'no-unexpected-multiline': 2,//避免多行表达式
     'no-unmodified-loop-condition': 2,
     'no-unneeded-ternary': [2, {
       'defaultAssignment': false
-    }],
-    'no-unreachable': 2,
+    }],//禁止不必要的嵌套 var isYes = answer === 1 ? true : false;
+    'no-unreachable': 2,//不能有无法执行的代码
     'no-unsafe-finally': 2,
     'no-unused-vars': [2, {
       'vars': 'all',
       'args': 'none'
-    }],
-    'no-useless-call': 2,
+    }],//不能有声明后未被使用的变量或参数
+    'no-useless-call': 2,//禁止不必要的call和apply
     'no-useless-computed-key': 2,
     'no-useless-constructor': 2,
     'no-useless-escape': 0,
     'no-whitespace-before-property': 2,
-    'no-with': 2,
+    'no-with': 2,//禁用with
     'one-var': [2, {
       'initialized': 'never'
-    }],
+    }],//连续声明
     'operator-linebreak': [2, 'after', {
       'overrides': {
         '?': 'before',
         ':': 'before'
       }
-    }],
-    'padded-blocks': [2, 'never'],
-    'quotes': [2, 'single', {
-      'avoidEscape': true,
-      'allowTemplateLiterals': true
-    }],
-    'semi': [2, 'never'],
+    }],//换行时运算符在行尾还是行首
+    'padded-blocks': 0,//块语句内行首行尾是否要空行
+    'quotes': 0,//引号类型 `` "" ''
+    'semi': 0,//语句强制分号结尾
     'semi-spacing': [2, {
       'before': false,
       'after': true
     }],
-    'space-before-blocks': [2, 'always'],
-    'space-before-function-paren': [2, 'never'],
-    'space-in-parens': [2, 'never'],
-    'space-infix-ops': 2,
-    'space-unary-ops': [2, {
+    'space-before-blocks': [0, 'always'],//不以新行开始的块{前面要不要有空格
+    'space-before-function-paren': [0, "always"],//函数定义时括号前面要不要有空格
+    'space-in-parens': [0, 'never'],//小括号里面要不要有空格
+    'space-infix-ops': 0,//中缀操作符周围要不要有空格
+    'space-unary-ops': [0, {
       'words': true,
       'nonwords': false
     }],
-    'spaced-comment': [2, 'always', {
-      'markers': ['global', 'globals', 'eslint', 'eslint-disable', '*package', '!', ',']
-    }],
+    "spaced-comment": 0,//注释风格要不要有空格什么的
     'template-curly-spacing': [2, 'never'],
-    'use-isnan': 2,
-    'valid-typeof': 2,
-    'wrap-iife': [2, 'any'],
+    'use-isnan': 2,//禁止比较时使用NaN，只能用isNaN()
+    'valid-typeof': 2,//必须使用合法的typeof的值
+    'wrap-iife': [2, 'any'],//立即执行函数表达式的小括号风格
     'yield-star-spacing': [2, 'both'],
-    'yoda': [2, 'never'],
-    'prefer-const': 2,
+    'yoda': [2, 'never'],//禁止尤达条件
+    'prefer-const': 0,//首选const
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'object-curly-spacing': [2, 'always', {
       objectsInObjects: false
-    }],
-    'array-bracket-spacing': [2, 'never']
+    }],//大括号内是否允许不必要的空格
+    'array-bracket-spacing': [2, 'never']//是否允许非空数组里面有多余的空格
   }
 }
